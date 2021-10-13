d61349c931b96fc3fa620cec3e641374ceb6442b
Teo <teocyp1@gmail.com>
2021-10-13 16:18:33 +0300

    fetch data prj

---
+++ Fetch Data Project/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import FetchData from './FetchData';
+
+function App() {
+  return (
+    <div className='App'>
+      <header>
+        <FetchData />
+      </header>
+    </div>
+  );
+}
+
+export default App;
